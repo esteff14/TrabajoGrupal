943b445438d09b36c61041bc559faa9c3b8fa854
Estefanía Sánchez <estefi2000ms2@gmail.com>
2023-12-05 09:07:18 -0500

    version1

---
+++ Figuras/src/Figuras.java
@@ -0,0 +1,25 @@
+public class Figuras {
+    float lado;
+    int numLados;
+
+    public Figuras(float lado, int numLados) {
+        this.lado = lado;
+        this.numLados = numLados;
+    }
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+    public int getNumLados() {
+        return numLados;
+    }
+
+    public void setNumLados(int numLados) {
+        this.numLados = numLados;
+    }
+}
