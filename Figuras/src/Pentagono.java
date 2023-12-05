2ee61c4e75b4f3c311fe9f57687fac61c0388f4b
Estefanía Sánchez <estefi2000ms2@gmail.com>
2023-12-05 09:50:23 -0500

    pentagono

--- Figuras/src/Pentagono.java
+++ Figuras/src/Pentagono.java
@@ -1,2 +1,22 @@
-public class Pentagono {
+public class Pentagono extends Irregulares {
+    double apotema;
+    public Pentagono(float lado, int numLados) {
+        super(lado, numLados);
+    }
+
+    public double getApotema() {
+        return apotema;
+    }
+
+    public void setApotema(double apotema) {
+        this.apotema = apotema;
+    }
+    double perimetroP= this.getLado()*5;
+    public void PerimetroPentagono(){
+
+        System.out.println("El perimetro del pentagono es %.2f\n: " + perimetroP);
+    }
+    public void AreaPentagono(){
+        System.out.println("El area del pentagono es: " + (perimetroP * getApotema())/2);
+    }
 }
