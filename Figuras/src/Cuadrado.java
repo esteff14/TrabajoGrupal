baaefbb0e4a93caf3a1d8b471bef913d64d7e85f
Alejo-P <marcelo.pinzon@epn.edu.ec>
2023-12-05 09:38:14 -0500

    Area y perimetro del cuadrado

--- Figuras/src/Cuadrado.java
+++ Figuras/src/Cuadrado.java
@@ -1,2 +1,18 @@
-public class Cuadrado {
+public class Cuadrado extends Bidimensional{
+    //Atributos
+    public Cuadrado (float lado, int numLados)
+    {
+        super(lado, numLados);
+    }
+    public void area()
+    {
+        double area=Math.pow(this.lado, 2);
+        System.out.printf("El area del cuadrado es: %.2f\n", area);
+    }
+
+    public void perimetro()
+    {
+        double perimetro=this.lado*4;
+        System.out.printf("El perimetro del cuadrado es: %.2f\n", perimetro);
+    }
 }
